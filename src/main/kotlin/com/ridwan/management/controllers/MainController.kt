8248d7c22092c63cd8f79408d402e00ea603acf9
Ridwan Adhi Pratama <ridwanadhip@gmail.com>
2019-02-07 11:15:54 +0700

    Add api for retrieving users

--- src/main/kotlin/com/ridwan/management/controllers/MainController.kt
+++ src/main/kotlin/com/ridwan/management/controllers/MainController.kt
@@ -3,10 +3,12 @@ package com.ridwan.management.controllers
 import com.ridwan.management.misc.ContentType
 import com.ridwan.management.ext.endAsEmptyJson
 import com.ridwan.management.ext.endAsErrorJson
+import com.ridwan.management.ext.endAsJson
 import com.ridwan.management.verticles.HttpServerVerticle
 import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.core.json.array
 import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.ext.sql.queryAwait
 import io.vertx.kotlin.ext.sql.updateWithParamsAwait
 import java.lang.Exception
 import java.util.*
@@ -14,11 +16,24 @@ import java.util.*
 class MainController(verticle: HttpServerVerticle) : Controller(verticle) {
     override fun setupRouter() {
         routeGet("/", this::indexAction)
+        routeGet("/get-users", this::getUsersAction)
         routePost("/add-user", this::addUserAction).consumes(ContentType.JSON)
     }
 
     private suspend fun indexAction(context: RoutingContext) {
         context.response().end("It works!")
+    }
+
+    private suspend fun getUsersAction(context: RoutingContext) {
+        val db = verticle.db
+        val result = try {
+            db.queryAwait("select username, email, role from users")
+        } catch (e: Exception) {
+            context.endAsErrorJson(500, "server error")
+            return
+        }
+
+        context.endAsJson(result.rows)
     }
 
     private suspend fun addUserAction(context: RoutingContext) {
